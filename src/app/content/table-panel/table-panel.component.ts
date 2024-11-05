06c92c0ec50aa979e739b7d06da8bf32a5029878
Andrew Palmer <PJIR@cms.hhs.gov>
2024-11-05 08:26:14 -0500

    add chart and table widgets

--- src/app/content/table-panel/table-panel.component.ts
+++ src/app/content/table-panel/table-panel.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { TableComponent } from './table/table.component';
+
 
 @Component({
   selector: 'app-table-panel',
   standalone: true,
-  imports: [MatExpansionModule, MatIconModule, MatFormFieldModule],
+  imports: [MatExpansionModule, MatIconModule, MatFormFieldModule, TableComponent],
   templateUrl: './table-panel.component.html',
   styleUrl: './table-panel.component.css'
 })
